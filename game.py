3ec0a2cb4172c04ee6200c1c91334247dc957c6d
Yulia Slepcova <yulik1995@gmail.com>
2020-04-22 14:42:46 +0300

    remote repository is cloned

---
+++ game.py
@@ -0,0 +1,22 @@
+number = 42
+
+while True:
+
+    answer = input('Enter number ')
+    
+    if not answer or answer == 'exit':
+        break
+    
+    if not answer.isdigit():
+        print('Enter the correct number ')
+        continue
+    
+    user_anser = int(answer)
+
+    if user_anser > number:
+        print('Our number is less')
+    elif user_anser < number:
+        print('Our number is bigger')
+    else:
+        print('Congratulations! You guessed the number', + number)
+        break
